175b1e5c527501d200a7f2684cc9378e8d361069
arikpar <43591314+arikpar@users.noreply.github.com>
2019-01-24 22:00:47 -0600

    Create chapter1case1.cs

---
+++ chapter1case1.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+namespace Ch1Case1
+{
+    class GreenvilleMotto
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("The stars shine in Greenville.");
+        }
+    }
+}
